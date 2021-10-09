6b6be7a637e396873fb5bfab1f12a10f87acde6d
Vicente Marçal <vicente.marcal@gmail.com>
2021-10-09 10:45:41 -0400

    todo-api-37 Created an endpoint to get detail by id from the database to User resource.

--- api/users/routers/business_rules.py
+++ api/users/routers/business_rules.py
@@ -1,8 +1,14 @@
 from api.users.data.users import UserData
-from api.users.models.users import UserModelOut
+from api.users.models.users import UserAllModelOut, UserDetailModelOut
+from api.utils.pydantic_oid import PyObjectId
 
 
-def get_all():
-    users = UserData.objects().as_pymongo()
-    users_list = [UserModelOut(**user) for user in users]
-    return users_list
+def get_users(user_id: PyObjectId = None):
+    if not user_id:
+        users = UserData.objects().as_pymongo()
+        users_response = [UserAllModelOut(**user) for user in users]
+    else:
+        user = UserData.objects(id=user_id).as_pymongo().first()
+        users_response = UserDetailModelOut(**user) if user else None
+
+    return users_response
--- api/users/routers/users.py
+++ api/users/routers/users.py
@@ -2,12 +2,18 @@ from typing import List
 
 from fastapi import APIRouter
 
-from api.users.models.users import UserModelOut
-from api.users.routers.business_rules import get_all
+from api.users.models.users import UserAllModelOut
+from api.users.routers.business_rules import get_users
+from api.utils.pydantic_oid import PyObjectId
 
 router = APIRouter(prefix='/api/v1/users', tags=['users'])
 
 
 @router.get('/')
-def get_all_users() -> List[UserModelOut]:
-    return get_all()
+def get_all_users() -> List[UserAllModelOut]:
+    return get_users()
+
+
+@router.get('/{user_id}')
+def get_user_detail(user_id: PyObjectId):
+    return get_users(user_id)
