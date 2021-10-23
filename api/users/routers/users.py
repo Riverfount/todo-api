f77569a73dfc22e70a41dabd0c94b1f4b6b7d53e
Vicente Marçal <vicente.marcal@gmail.com>
2021-10-23 09:41:30 -0400

    todo-api-38 Created an endpoint to insert a new user on the database to User resource.

--- api/users/routers/users.py
+++ api/users/routers/users.py
@@ -2,8 +2,8 @@ from typing import List
 
 from fastapi import APIRouter
 
-from api.users.models.users import UserAllModelOut
-from api.users.routers.business_rules import get_users
+from api.users.models.users import UserAllModelOut, UserDetailModelOut, UserModelIn
+from api.users.routers.business_rules import get_users, insert_user
 from api.utils.pydantic_oid import PyObjectId
 
 router = APIRouter(prefix='/api/v1/users', tags=['users'])
@@ -17,3 +17,8 @@ def get_all_users() -> List[UserAllModelOut]:
 @router.get('/{user_id}')
 def get_user_detail(user_id: PyObjectId):
     return get_users(user_id)
+
+
+@router.post('/', status_code=201)
+def insert_new_user(user: UserModelIn) -> UserDetailModelOut:
+    return insert_user(user)
